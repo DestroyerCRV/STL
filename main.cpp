d8c2d593174a53a7d4af4deb96151ccf02909415
DestroyerCRV <g.david.6227@gmail.com>
2024-10-29 18:01:23 +0200

    My first push

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+// this is the main function
 
+struct problems {
+    string problema;
+    string specialitateNecesara;
+
+
+};
+
+struct doctors {
+    string nume;
+    string specializare;
+
+};
 int main()
 {
+
+    vector<problems> probleme;
+    vector<doctors> doctori;
     ifstream inFile("input.txt");
 
     int no_problems, no_doctors;
@@ -17,17 +34,38 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        probleme.emplace_back(name, speciality);
     }
 
     inFile >> no_doctors;
+
+
 
     for (int i = 0; i < no_doctors; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctori.emplace_back(name, speciality);
     }
+
+    for (auto& i : probleme)
+    {
+        bool found = false;
+        for (auto& j : doctori)
+        {
+            if (i.specialitateNecesara == j.specializare)
+            {
+                found = true;
+                cout << i.problema << " " << "Acceptat\n";
+                break;
+            }
+            
+        }
+        if(!found)
+            cout << i.problema << " " << "Respins\n";
+    }
+
+
 
     return 0;
 }
